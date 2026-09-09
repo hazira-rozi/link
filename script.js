82a2697f5c58d773c4ae1629230ee778704d835f
hazira-rozi <hazirafakhrurrozi@hotmail.com>
2026-09-09 10:11:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // ==========================================
 const databaseLink = {
     
+    "crud PHP": "https://docs.google.com/document/d/11AiLcdflFrR1m65_PbSqkoOBXwqMkncg/edit",
     "tugasphp": "https://drive.google.com/file/d/19qmfRFgTi9Mdp8WdF_B240m1reF9q3st/view?usp=sharing",
     "modullengkap": "https://drive.google.com/drive/folders/1Mxf25yssu9bB-AD6ok9XcUUycbHINPug?usp=sharing"
 };
